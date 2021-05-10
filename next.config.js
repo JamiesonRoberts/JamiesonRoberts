f73abff8082d3a8974d65a2a062f9fbccf023733
Jamieson Roberts <hello@jamiesonroberts.com>
2021-05-10 10:42:13 -0400

    Adding in sentry baseline

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const { withSentryConfig } = require('@sentry/nextjs')
+
+const moduleExports = {
     i18n: {
         // These are all the locales you want to support in
         // your application
@@ -20,3 +22,15 @@ module.exports = {
         webpack5: true,
     },
 }
+
+const SentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
