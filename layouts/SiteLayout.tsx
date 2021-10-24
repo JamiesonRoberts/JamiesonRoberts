82cc01f327e77c9acc409ec2de80831e800f60a0
Jamieson Roberts <hello@jamiesonroberts.com>
2021-10-24 12:49:20 -0700

    Reset

--- layouts/SiteLayout.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from 'react'
-
-export default function SiteLayout({ children }: { children: ReactNode }) {
-  return <div className="test">{children}</div>
-}
--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // Run type-check on changes to TypeScript files
-  '**/*.ts?(x)': () => 'npm type-check',
-  // Run ESLint on changes to JavaScript/TypeScript files
-  '**/*.(ts|js)?(x)': (filenames) => `npm lint ${filenames.join(' ')}`,
-}
