163fcea3c08d8746dff631fb440773853f119c59
Jamieson Roberts <hello@jamiesonroberts.com>
2021-10-24 17:36:35 -0700

    Adding in prettier and reforamting baseline

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,19 @@
-import React from "react";
-import App from "next/app";
-import PlausibleProvider from "next-plausible";
+import React from 'react'
+import App from 'next/app'
+import PlausibleProvider from 'next-plausible'
 
 class MyApp extends App {
-  render() {
-    const {
-      Component,
-      pageProps
-    } = this.props;
-    return (
-      <PlausibleProvider domain="jamiesonroberts.com" trackOutboundLinks={true}>
-        <Component {...pageProps} />
-      </PlausibleProvider>
-    );
-  }
+    render() {
+        const {
+            Component,
+            pageProps
+        } = this.props
+        return (
+            <PlausibleProvider domain='jamiesonroberts.com' trackOutboundLinks={true}>
+                <Component {...pageProps} />
+            </PlausibleProvider>
+        )
+    }
 }
 
-export default MyApp;
+export default MyApp
