86ca1e154c6d805a222be7f2d24c92e9a4d5a5e3
Jamieson Roberts <hello@jamiesonroberts.com>
2022-10-31 09:26:04 -0400

    Policy page basic mta

--- pages/policies.js
+++ pages/policies.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import GenericHeader from '../components/GenericHeader'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
@@ -6,16 +8,28 @@ import styles from  '../styles/pages/policies.module.css'
 
 function Home () {
     return (
-        <div className={styles.layout}>
-            <GenericHeader className={styles.header} data-color-scheme={'light'}>
-                <h1>Policies</h1>
-            </GenericHeader>
-            <Main className={styles.main} data-color-scheme={'dark'}>
-                <h2>Accessibility Policy</h2>
-                <h2>Privacy and Data Removal</h2>
-            </Main>
-            <Footer className={styles.footer} data-color-scheme={'dark'} />
-        </div>
+        <>
+            <Head>
+                <title>
+                    {`Jamieson Roberts | Digital Policies`}
+                </title>
+                <meta
+                    name={'description'}
+                    content={`You know the drill, here are the digital polices for the website including privacy and accessibility.`}
+                    key={'desc'}
+                />
+            </Head>
+            <div className={styles.layout}>
+                <GenericHeader className={styles.header} data-color-scheme={'light'}>
+                    <h1>Policies</h1>
+                </GenericHeader>
+                <Main className={styles.main} data-color-scheme={'dark'}>
+                    <h2>Accessibility Policy</h2>
+                    <h2>Privacy and Data Removal</h2>
+                </Main>
+                <Footer className={styles.footer} data-color-scheme={'dark'} />
+            </div>
+        </>
     )
 }
 
