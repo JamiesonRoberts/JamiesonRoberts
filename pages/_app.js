5911a91b1eec6d8f76d2f6bd3f24fcf1917bbb2c
Jamieson Roberts <hello@jamiesonroberts.com>
2022-10-31 00:26:39 -0400

    Microdata attempt

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,129 @@ const SiteLayout = ({ children }) => {
     return (
         <div className={roboto.variable}>
             {children}
+            <script
+                type='application/ld+json'
+                dangerouslySetInnerHTML={{
+                    __html: `{
+                        "@context": "http://www.schema.org",
+                        "@type": "Person",
+                        "@id": "https://jamiesonroberts.com/#person",
+                        "name": "Jamieson Roberts",
+                        "nationality": "Canadian",
+                        "hasOccupation": {
+                            "@id": "#occupation"
+                        },
+                        "hasOfferCatalog": {
+                            "@id": "#OfferCatalog"
+                        },
+                        "birthPlace": {
+                            "@type": "Place",
+                            "address": {
+                                "@type": "PostalAddress",
+                                "addressLocality": "London",
+                                "addressRegion": "ON",
+                                "addressCountry": "Canada"
+                            }
+                        },
+                        "alumniOf": [
+                            {
+                                "@type": "CollegeOrUniversity",
+                                "name": "Western University",
+                                "sameAs": "https://www.uwo.ca/"
+                            }
+                        ],
+                        "gender": "Male",
+                        "description": "Digital Creator",
+                        "jobTitle": "Senior Front End Developer",
+                        "worksFor": [
+                            {
+                                "@type": "Organization",
+                                "name": "SAXX Underwear Co.",
+                                "sameAs": [
+                                    "https://twitter.com/saxxunderwear"
+                                ]
+                            }
+                        ],
+                        "workLocation": [
+                        ],
+                        "url": "https://jamiesonroberts.com",
+                        "image": "TBD",
+                        "address": {
+                            "@type": "PostalAddress",
+                            "addressLocality": "London",
+                            "addressRegion": "ON",
+                            "addressCountry": "Canada"
+                        },
+                        "sameAs": [
+                            "https://twitter.com/jamiesonroberts"
+                        ]
+                    }`
+                }}
+                key={'Person-jsonld'}
+            />
+            <script
+                type='application/ld+json'
+                dangerouslySetInnerHTML={{
+                    __html: `{
+                        "@context": "https://schema.org",
+                        "@type": "OfferCatalog",
+                        "@id": "#OfferCatalog",
+                        "name": "Services",
+                        "itemListElement": [
+                            {
+                                "@type": "OfferCatalog",
+                                "name": "Web development and related services",
+                                "itemListElement": [
+                                    {
+                                        "@type": "Offer",
+                                        "itemOffered": {
+                                            "@type": "Service",
+                                            "name": "Web Development"
+                                        }
+                                    },
+                                    {
+                                        "@type": "Offer",
+                                        "itemOffered": {
+                                            "@type": "Service",
+                                            "name": "Software Development"
+                                        }
+                                    },
+                                    {
+                                        "@type": "Offer",
+                                        "itemOffered": {
+                                            "@type": "Service",
+                                            "name": "Technical SEO"
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }`
+                }}
+                key={'OfferCatalog-jsonld'}
+            />
+            <script
+                type='application/ld+json'
+                dangerouslySetInnerHTML={{
+                    __html: `{
+                        '@context': 'http://schema.org',
+                        '@type': 'WebSite',
+                        '@id': 'https://jamiesonroberts.com/#website',
+                        'url': 'https://jamiesonroberts.com',
+                        'image': 'TBD',
+                        'inLanguage': 'en-CA',
+                        'author': 'Jamieson Roberts',
+                        'keywords': 'Jamieson Roberts',
+                        'accountablePerson': {
+                            '@id': 'https://jamiesonroberts.com/#person'
+                        },
+                        'about': {
+                            '@id': 'https://jamiesonroberts.com/#person'
+                        }
+                    }`
+                }}
+                key={'Website-jsonld'}
+            />
         </div>
     )
 }
