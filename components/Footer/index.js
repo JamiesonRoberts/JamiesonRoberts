d907431c5eba1dbe44d1969197604dba42054345
Jamieson Roberts <hello@jamiesonroberts.com>
2023-01-11 22:58:40 -0500

    Working through simplification and getting content going

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,12 +1,26 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import badge from '../../public/jr-badge.png'
 
-export default function Header (props) {
+export default function Header(props) {
     return (
-        <footer {...props}
-                className={props.className}
-                role={'contentinfo'}
-        >
-            &copy;2022 Jamieson Roberts. <Link href={'/policies'}>Policies</Link> | <a href={'https://plausible.io/jamiesonroberts.com/'} target={'_blank'} rel="noopener noreferrer">Analytics</a>
+        <footer {...props} className={props.className} role={'contentinfo'}>
+            <Link
+                href={'https://plausible.io/jamiesonroberts.com/'}
+                target={'_blank'}
+                rel={'noopener noreferrer'}
+                alt={'View open analytics data about this site'}
+            >
+                Analytics
+            </Link>
+            <p>Copyright &copy; 2023 Jamieson Roberts.</p>
+            <Image
+                src={badge}
+                alt='A blue circle with the letters JR over top of the background.'
+                width={75}
+                height={75}
+                priority={false}
+            />
         </footer>
     )
 }
