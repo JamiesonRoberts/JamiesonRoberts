ff2390812a2211cd033d2392c465d2fe92d7b17b
Jamieson Roberts <hello@jamiesonroberts.com>
2023-01-13 10:29:14 -0500

    Finishing up sitemap/robots

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -22,9 +22,10 @@ async function generateSitemap() {
         '!pages/api',
         '!pages/404.js',
     ])
-    const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages.map(addPage).join('\n')}
-</urlset>`
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages.map(addPage).join('\n')}
+    </urlset>`
 
     fs.writeFileSync('public/sitemap.xml', sitemap)
 }
