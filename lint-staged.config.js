1ab6c1042a0b063377853fa9ac9af1f26e30f76e
Jamieson Roberts <hello@jamiesonroberts.com>
2026-03-21 13:59:29 -0400

    Cleaning up and getting stylelint configured

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
   '*.{js,mjs,cjs,astro}': ['eslint --fix', 'prettier --write'],
-  // '*.{css,scss,sass}': ['stylelint --fix', 'prettier --write'],
+  '*.{css,scss,sass,astro}': ['stylelint --fix', 'prettier --write'],
   '*.{md,json}': 'prettier --write',
 }
